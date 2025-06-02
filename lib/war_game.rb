2eb81a2b27a27596715d260b70f9890717e46cd7
Luke Simmons <lucasjsimmons@yahoo.com>
2025-06-02 15:07:54 -0400

    Add start, winner, and play_round

--- lib/war_game.rb
+++ lib/war_game.rb
@@ -6,16 +6,28 @@
 
   def initialize
     @deck = CardDeck.new
-    @player1 = WarPlayer.new
-    @player2 = WarPlayer.new
-
-    deck.shuffle
+    @player1 = WarPlayer.new('P1')
+    @player2 = WarPlayer.new('P2')
   end
 
   def deal_cards
-    26.times do
-      player1.hand << deck.deal
-      player2.hand << deck.deal
+    (CardDeck::BASE_DECK_SIZE / 2).times do
+      player1.add_card(deck.deal)
+      player2.add_card(deck.deal)
     end
+  end
+
+  def start
+    deck.shuffle!
+    deal_cards
+  end
+
+  def winner
+    return player1 if player2.hand.empty?
+    return player2 if player1.hand.empty?
+  end
+
+  def play_round
+    "P1 plays #{player1.play_card.rank} of #{player1.play_card.suit} | P2 plays #{player2.play_card.rank} of #{player2.play_card.suit}"
   end
  end
