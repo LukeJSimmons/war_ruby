813edd1e2b427b5d5abf81610e7e0b8ab9761ebb
Luke Simmons <lucasjsimmons@yahoo.com>
2025-06-02 13:34:40 -0400

    Add Player play_card

--- lib/war_player.rb
+++ lib/war_player.rb
@@ -1,8 +1,12 @@
  
 class WarPlayer
   attr_reader :hand
-  
-  def initialize
-    @hand = []
+
+  def initialize(hand=[])
+    @hand = hand
+  end
+
+  def play_card
+    @hand.pop
   end
 end
--- spec/war_player_spec.rb
+++ spec/war_player_spec.rb
@@ -5,4 +5,12 @@ describe 'WarPlayer' do
     war_player = WarPlayer.new
     expect(war_player).to respond_to :hand
   end
+
+  it 'plays top card in hand' do
+    war_player = WarPlayer.new([PlayingCard.new('A','H')])
+    top_card = war_player.hand[0]
+    played_card = war_player.play_card
+    expect(played_card).to eq top_card
+    expect(played_card).to respond_to :suit
+  end
 end
