306af54ad7b5187917a68f3b11c4ba7ec4c936e0
Luke Simmons <lucasjsimmons@yahoo.com>
2025-06-03 11:03:24 -0400

    Start refactor on play_round

--- lib/war_game.rb
+++ lib/war_game.rb
@@ -28,27 +28,26 @@
   end
 
   def play_round(cards=[])
-    message = ""
-
     p1_card = player1.play_card
     p2_card = player2.play_card
-    
     cards.push(p1_card, p2_card).shuffle!
-
-    if p1_card.has_greater_value_than?(p2_card)
+    winner = if p1_card.has_greater_value_than?(p2_card)
       cards.each { |card| player1.add_card(card) }
-      message += "Player 1"
+      "Player 1"
     elsif p2_card.has_greater_value_than?(p1_card)
       cards.each { |card| player2.add_card(card) }
-      message += "Player 2"
+      "Player 2"
     else
       play_round(cards) unless player1.hand.empty? || player2.hand.empty?
     end
+    display_message(winner, cards)
+  end
 
-    message += " took "
+  def display_message(winner, cards)
+    message = ""
+    message += "#{winner} took "
     cards.each_with_index { |card, index| message += "#{card.rank} of #{card.suit} with " unless index == cards.count-1 }
-    message += "#{cards[-1].rank} of #{cards[-1].suit}"
-    cards = []
+    message += "#{"#{cards[-1].rank} of #{cards[-1].suit}"}"
     message
   end
  end
--- spec/war_game_spec.rb
+++ spec/war_game_spec.rb
@@ -1,76 +1,74 @@
 require_relative '../lib/war_game'
 
 describe 'WarGame' do
+
+  before :each do
+    @game = WarGame.new
+  end
   it 'initalizes with deck and players' do
-    game = WarGame.new
-    expect(game).to respond_to :deck
-    expect(game).to respond_to :player1
-    expect(game).to respond_to :player2
+    expect(@game).to respond_to :deck
+    expect(@game).to respond_to :player1
+    expect(@game).to respond_to :player2
   end
 
   it 'deals half the deck to each player' do
-    game = WarGame.new
-    game.deal_cards
-    expect(game.player1.hand.length).to eq 26
-    expect(game.player2.hand.length).to eq 26
+    @game.deal_cards
+    expect(@game.player1.hand.length).to eq 26
+    expect(@game.player2.hand.length).to eq 26
   end
 
   it 'returns player1 if he is the winner' do
-    game = WarGame.new
     p1_card = PlayingCard.new('A','Hearts')
-    game.player1.add_card(p1_card)
-    expect(game.winner).to eq game.player1
-    expect(game.winner).to_not eq game.player2
+    @game.player1.add_card(p1_card)
+    expect(@game.winner).to eq @game.player1
+    expect(@game.winner).to_not eq @game.player2
   end
 
   it 'gives P2 card to P1 if P1 wins round' do
-    game = WarGame.new
     p1_card = PlayingCard.new('A','Hearts')
     p2_card = PlayingCard.new('2','Hearts')
-    game.player1.add_card(p1_card)
-    game.player2.add_card(p2_card)
-    game.play_round
-    expect(game.player1.hand.count).to eq 2
-    expect(game.player2.hand.count).to eq 0
+    @game.player1.add_card(p1_card)
+    @game.player2.add_card(p2_card)
+    @game.play_round
+    expect(@game.player1.hand.count).to eq 2
+    expect(@game.player2.hand.count).to eq 0
   end
 
   it 'handles a tie properly' do
-    game = WarGame.new
-    game.player1.add_card(PlayingCard.new('A', 'Clubs'))
-    game.player2.add_card(PlayingCard.new('A', 'Hearts'))
-    game.player1.add_card(PlayingCard.new('2','Hearts'))
-    game.player2.add_card(PlayingCard.new('10','Hearts'))
-    game.play_round
-    expect(game.player1.hand.count).to eq 0
-    expect(game.player2.hand.count).to eq 4
+    @game.player1.add_card(PlayingCard.new('A', 'Clubs'))
+    @game.player2.add_card(PlayingCard.new('A', 'Hearts'))
+    @game.player1.add_card(PlayingCard.new('2','Hearts'))
+    @game.player2.add_card(PlayingCard.new('10','Hearts'))
+    @game.play_round
+    expect(@game.player1.hand.count).to eq 0
+    expect(@game.player2.hand.count).to eq 4
   end
 
   it 'displays an overview message each round' do
-    game = WarGame.new
     p1_card = PlayingCard.new('A','Hearts')
     p2_card = PlayingCard.new('2','Hearts')
-    game.player1.add_card(p1_card)
-    game.player2.add_card(p2_card)
-    expect(game.play_round).to eq "Player 1 took A of Hearts with 2 of Hearts"
+    @game.player1.add_card(p1_card)
+    @game.player2.add_card(p2_card)
+    output = @game.play_round
+    expect(output).to include "A of Hearts"
+    expect(output).to include "2 of Hearts"
   end
 
   it 'displays tie message when players tie' do
-    game = WarGame.new
-    p1_card = PlayingCard.new('A','Hearts')
-    p2_card = PlayingCard.new('2','Hearts')
-    p1_card = PlayingCard.new('A','Clubs')
-    p2_card = PlayingCard.new('A','Diamonds')
-    game.player1.add_card(p1_card)
-    game.player2.add_card(p2_card)
-    expect(game.play_round).to eq " took A of Clubs with A of Diamonds"
+    p1_cards = [PlayingCard.new('A','Clubs'),PlayingCard.new('A','Hearts')]
+    p2_cards = [PlayingCard.new('2','Hearts'),PlayingCard.new('A','Diamonds')]
+    @game.player1.add_card(p1_cards)
+    @game.player2.add_card(p2_cards)
+    output = @game.play_round
+    expect(output).to include "A of Clubs"
+    expect(output).to include "A of Diamonds"
   end
 
   it 'can play a whole game' do
-    game = WarGame.new
-    game.start
-    until game.winner do
-      puts game.play_round
+    @game.start
+    until @game.winner do
+      puts @game.play_round
     end
-    expect(game.winner).to respond_to :hand
+    expect(@game.winner).to respond_to :hand
   end
 end
