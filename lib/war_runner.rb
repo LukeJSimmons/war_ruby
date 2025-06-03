ad440fd81a049d0dfb0b59c56e6197de150b994c
Luke Simmons <lucasjsimmons@yahoo.com>
2025-06-03 12:15:35 -0400

    Add most rounds output option

--- lib/war_runner.rb
+++ lib/war_runner.rb
@@ -13,8 +13,10 @@ end
 
 win_ratio = "Win Ratio | #{(games.select { |game| game.winner.name == 'Player 1' }.count)/(total_games/100).to_f}%"
 average_rounds = "Average Rounds | #{games.map(&:rounds).reduce(0) { |sum, num| sum + num } / games.count}"
+most_rounds = "Most Rounds | #{games.map(&:rounds).max}"
 infinite_games = "Infinite Games | #{(games.select { |game| game.rounds > 5000 }.count)/(total_games/100).to_f}%"
 
 puts win_ratio
 puts average_rounds
+puts most_rounds
 puts infinite_games
