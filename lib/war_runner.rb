3e9da3468ba0e48b8bdd00971700a955fc228bea
Luke Simmons <lucasjsimmons@yahoo.com>
2025-06-03 12:11:22 -0400

    Add infinite games option to output, allow for infinite games to test

--- lib/war_runner.rb
+++ lib/war_runner.rb
@@ -1,8 +1,9 @@
 require_relative 'war_game'
 
+total_games = 1000
 games = []
 
-100.times do
+total_games.times do
   game = WarGame.new
   game.start
   puts game.play_round until game.winner
@@ -10,8 +11,10 @@ games = []
   games << game
 end
 
-win_ratio = "Win Ratio | #{games.select { |game| game.winner.name == 'Player 1' }.count}%"
+win_ratio = "Win Ratio | #{(games.select { |game| game.winner.name == 'Player 1' }.count)/(total_games/100).to_f}%"
 average_rounds = "Average Rounds | #{games.map(&:rounds).reduce(0) { |sum, num| sum + num } / games.count}"
+infinite_games = "Infinite Games | #{(games.select { |game| game.rounds > 5000 }.count)/(total_games/100).to_f}%"
 
 puts win_ratio
 puts average_rounds
+puts infinite_games
