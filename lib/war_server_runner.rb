cd3c367a5eff03c658f466d30392086db587d1a1
Luke Simmons <lucasjsimmons@yahoo.com>
2025-06-04 13:51:32 -0400

    Fix accept_new_client name error, add startup message

--- lib/war_server_runner.rb
+++ lib/war_server_runner.rb
@@ -2,8 +2,9 @@ require_relative 'war_socket_server'
 
 server = WarSocketServer.new
 server.start
+puts "Server #{server} has been started on port #{server.port_number}"
 loop do
-  server.accept_client
+  server.accept_new_client
   game = server.create_game_if_possible
   if game
     server.run_game(game)
